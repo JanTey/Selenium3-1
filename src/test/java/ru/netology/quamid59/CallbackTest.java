d651ad19e56c3fb1c4f2c241f0fa23344a184afa
Zhan Teivans <118687635+JanTey@users.noreply.github.com>
2023-07-01 11:26:32 +0300

    Update CallbackTest.java

--- src/test/java/ru/netology/quamid59/CallbackTest.java
+++ src/test/java/ru/netology/quamid59/CallbackTest.java
@@ -28,7 +28,7 @@ public class CallBackTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
 
