27dda7323bc0c9d481c52b68202a28b14cf530b5
Zhan Teyvans <teyvans@gmail.com>
2023-07-03 13:16:56 +0300

    Modified tests

--- src/test/java/ru/netology/quamid59/CallbackTest.java
+++ src/test/java/ru/netology/quamid59/CallbackTest.java
@@ -1,19 +1,18 @@
 package ru.netology.quamid59;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CallBackTest {
+class CallBackTest {
     private WebDriver driver;
 
     @BeforeAll
